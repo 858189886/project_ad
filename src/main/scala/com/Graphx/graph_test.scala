b116b108fda735eb86619673c20ff4d048f5d2dd
858189886zgh <858189886@qq.com>
2019-08-28 20:02:04 +0800

    完成

--- src/main/scala/com/Graphx/graph_test.scala
+++ src/main/scala/com/Graphx/graph_test.scala
@@ -2,14 +2,21 @@ package com.Graphx
 
 import org.apache.spark.graphx.{Edge, Graph, VertexId, VertexRDD}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object graph_test {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[*]")
     val sc: SparkContext = new SparkContext(conf)
+
+    // 我们要采取snappy压缩方式， 因为咱们现在用的是1.6版本的spark，到2.0以后呢，就是默认的了
+    // 可以不需要配置
+    val sqlContext = new SQLContext(sc)
+    sqlContext.setConf("spark.io.compression.snappy.codec", "snappy")
+
     //构造点的集合
-    val vertexRDD = sc.makeRDD(Seq(
+    val pointRDD = sc.makeRDD(Seq(
       (1L, ("詹姆斯", 35)),
       (2L, ("霍华德", 34)),
       (6L, ("杜兰特", 31)),
@@ -38,14 +45,30 @@ object graph_test {
 
     ))
     //构件图
-    val graph: Graph[(String, Int), Int] = Graph(vertexRDD, edge)
+    val graph: Graph[(String, Int), Int] = Graph(pointRDD, edge)
+
     //取出每个边上的最大顶点
     val vertices: VertexRDD[VertexId] = graph.connectedComponents().vertices
-    vertices.join(vertexRDD).map {
-      case (userId, (conId, (name, age))) => {
-        (conId, List(name, age))
+    //vertices.foreach(println)
+
+    /**
+      * RDD[(VertexId01, (VertexId02, (String,Int)))]:
+      *     VertexId01:原来的点的id
+      *     VertexId02:是最大点的id
+      *     (String,Int): 原始的数据
+      */
+
+    val vertexIdAndMaxpoinAndvalue: RDD[(VertexId, (VertexId, (String, Int)))] = vertices.join(pointRDD)
+    //temp.foreach(println)
+
+    val MaxpoinAndValue: RDD[(VertexId,List[(String,Int)])] = vertexIdAndMaxpoinAndvalue.map {
+      case (userId01, (conId02, (name, age))) => {
+        (conId02, List((name, age)))
       }
-    }.reduceByKey(_ ++ _).foreach(println)
+    }
+    val ans: RDD[(VertexId, List[(String, Int)])] = MaxpoinAndValue.reduceByKey(_ ++ _)
+     ans.foreach(println)
+    sc.stop()
 
   }
 }
